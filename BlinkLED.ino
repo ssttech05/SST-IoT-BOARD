fa111a74980a18d7ce6392b6b9dbc43d91c887a9
ssttech05 <99236421+ssttech05@users.noreply.github.com>
2022-02-08 11:13:42 +0530

    Blink LED  file created
    Blink LED Program using SST-Node MCU Board. Pin Number GPIO15 is used.

---
+++ BlinkLED.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(15,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(15,HIGH);
+  delay(1000);
+  digitalWrite(15,LOW);
+  delay(1000);
+  }  
